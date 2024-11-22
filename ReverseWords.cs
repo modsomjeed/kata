3a9f5903c8e27ac394d90e77557d7a0713417ea5
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 08:39:34 +0700

    training reverse words

---
+++ ReverseWords.cs
@@ -0,0 +1,21 @@
+namespace CodeWars;
+
+public class ReverseWords
+{
+    // Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+    // Examples
+    // "This is an example!" ==> "sihT si na !elpmaxe"
+    // "double  spaces"      ==> "elbuod  secaps"
+
+    public static string ReverseWords(string str)
+    {
+        var words = str.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] charArray = words[i].ToCharArray();
+            Array.Reverse(charArray);
+            words[i] = new string(charArray);
+        }
+        return string.Join(" ", words);
+    }
+}
