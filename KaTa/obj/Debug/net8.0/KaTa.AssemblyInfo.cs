6a2070ebe90635bd90cdf86cfa6ed015d19e8a9b
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 09:07:00 +0700

    training printer error

--- KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
+++ KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7029493b60a20860b2af081a690cb0faf3b24f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee879734e429f1fd9862173a60df2238c5cdcca")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
