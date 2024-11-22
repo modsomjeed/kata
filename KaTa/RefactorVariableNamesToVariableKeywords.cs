880d8ecb43dfb916f2243337c34b461e7f9c1ea7
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 11:09:31 +0700

    training - refactor variable names to variable keyword

---
+++ KaTa/RefactorVariableNamesToVariableKeywords.cs
@@ -0,0 +1,35 @@
+namespace CodeWars.KaTa
+{
+    public class VariableName
+    {
+        // Refactor this variables. Good luck ^_~
+        public bool @bool = false;
+        public char @char = '\0';
+        public sbyte @sbyte = 0;
+        
+        public byte @byte = 0;
+        public short @short = 0;
+        public ushort @ushort = 0;
+        public int @int = 0;
+        public uint @uint = 0;
+        public long @long = 0;
+        public ulong @ulong = 0;
+        public float @float = 0;
+        public double @double = 0;
+        public decimal @decimal = 0;
+        
+        // Information
+        // Define the following 13 variables:
+        // bool, char, sbyte, byte, short, ushort, int, uint, long, ulong, float, double, decimal
+
+        // The type of each variable must be the same as the name.
+        // The values don't matter.
+
+        // Examples:
+        // bool bool = false;
+        // char char = '\0';
+        // sbyte sbyte = 0;
+        
+
+    }
+}
