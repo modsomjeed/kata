084bcd23c3b9ac0c321e561cc44427c3be75442d
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 10:11:07 +0700

    training - lario and muigi problem

---
+++ KaTa/LarioAndMuigiPipeProblem.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Fixer
+{
+  public static List<int> PipeFix(List<int> numbers)
+  {
+    // Issue
+    // Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+    // The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+    // The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+    // Task
+    // Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+    // Example
+    // Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+    List<int> fixedPipes = new List<int>();
+    for (int i = numbers.Min(); i <= numbers.Max(); i++)
+    {
+        fixedPipes.Add(i);
+    }
+    return fixedPipes;
+  }
+}
