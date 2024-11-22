cf15b45a5ab86a5347fcb5ee1ae76791f9e5f6ae
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 09:29:53 +0700

    training decode the morse code

--- KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
+++ KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af30d2d3a8fdda1ecdb97f939f5daf86ec618c61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799cc66d73f83c8b473a2470d0d48d76046e7672")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
