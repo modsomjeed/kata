b263ef074ec21f63d7bb7739a163fe199d907755
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 09:51:38 +0700

    training - rgb to hex conversion

--- KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
+++ KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9739580a8435cddba1b35fa231e48e5466caba9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440a853177b7362558df386d97496eabf6b3a7c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
