beef44961d3038241053f27f22a8d08be8cd3b55
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 10:50:49 +0700

    training - convert a number to a string

---
+++ KaTa/ConvertANumberToAString.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    // We need a function that can transform a number (integer) into a string.
+
+    // What ways of achieving this do you know?
+
+    // Examples (input --> output):
+    // 123  --> "123"
+    // 999  --> "999"
+    // -100 --> "-100"
+
+    return num.ToString();
+  }
+}
