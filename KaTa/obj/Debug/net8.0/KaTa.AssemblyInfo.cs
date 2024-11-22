a718840a9d9128d14a63af17b06ee18a825dae53
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 10:31:01 +0700

    training - stop gninnip s my s drow

--- KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
+++ KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29a8265c6079355d552c2a9bca3808ffb8e169ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39daea5f22263f7e5c2c707d04d1a46b90b513d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
