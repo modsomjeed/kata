2dc0b2226b63fdc971b2434fe466ac3788abe3e2
ModSomJEED59 <modsomjeed59@192.168.1.13>
2024-11-22 11:11:08 +0700

    training - decibel scale

--- KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
+++ KaTa/obj/Debug/net8.0/KaTa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ee047a39e99d3b00e87c2940e975dbb48a2728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880d8ecb43dfb916f2243337c34b461e7f9c1ea7")]
 [assembly: System.Reflection.AssemblyProductAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KaTa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
